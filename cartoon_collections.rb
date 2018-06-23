3cbe8d0defdc7643cacaa383065f6984c9ab458a
paulheisele@gmail.com <paulheisele@gmail.com>
2018-06-23 15:49:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,12 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  if planeteer_calls.find {|x| x.length > 4}.class = string
+    true 
+  else 
+    false 
+  end 
 end
 
 def find_the_cheese# code an argument here
